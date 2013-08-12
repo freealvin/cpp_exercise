6a9e4a54645f14f24a845ad44c2958787a067a00
freealvin <freealvin@sina.com>
2013-08-12 18:37:40 +0800

    overload +

--- cpp3/03operator_overload.cpp
+++ cpp3/03operator_overload.cpp
@@ -6,17 +6,37 @@ class Fraction{
 	int d;
 	public:
 	Fraction(int n=0, int d=1):n(n), d(d){}
+	const Fraction operator+(const Fraction &f)const;
 	friend istream &operator>>(istream &in, Fraction &f);
 	friend ostream &operator<<(ostream &out, Fraction &f);
 };
 
+//求最大公约数
+int gcd(int a, int b)
+{
+	if(b==0) return a;
+	return gcd(b, a%b);
+}
+//作为函数成员，重载+运算符
+const Fraction Fraction::operator+(const Fraction &f)const
+{
+	Fraction fra;
+	fra.n = (f.n)*d + n*(f.d);
+	fra.d = d* (f.d);
+	int m = gcd(fra.n, fra.d);
+	fra.n /= m;
+	fra.d /= m;
+	cout<<(fra.n)<<'/'<<(fra.d)<<endl;
+	return fra;
+}
+//重载>>，以便于输入
 istream &operator>>(istream &in , Fraction &f)
 {
 	char c;
 	in>> f.n >> c >> f.d;
 	return in;
 }
-
+//重载<<，便于输出
 ostream &operator<<(ostream &out, Fraction &f)
 {
 	out<< f.n << '/' << f.d << endl;
@@ -26,8 +46,10 @@ ostream &operator<<(ostream &out, Fraction &f)
 
 int main()
 {
-	Fraction f;
-	cin>>f;
-	cout<<f;
+	Fraction f1;
+	Fraction f2;
+	cin>>f1>>f2;
+	cout<<f1<<f2;
+	//cout<<(f1+f2);
 	return 0;
 }
