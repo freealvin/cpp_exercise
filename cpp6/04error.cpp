a63f6170f5bbac85837ac44edf247abc146a155d
freealvin <freealvin@sina.com>
2013-09-08 13:17:00 +0800

    clear

--- cpp6/04error.cpp
+++ cpp6/04error.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 //io对象要求能转换成bool类型。对处于正常状态的io对象转成true，对处于错误状态的io对象转成falsiiiiie
+
+//处于错误状态的io对象拒绝执行io操作，。clear()只清除错误状态，但不清除缓冲区，这样此io对象可以继续执行io操作
 int main()
 {
 	cout << "cin = " << cin << endl;
