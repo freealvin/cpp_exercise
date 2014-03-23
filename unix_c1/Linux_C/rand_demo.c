2a1f5ec8979a3261d3607b24a794416cb4d968fb
freealvin <freealvin@sina.com>
2014-03-23 18:38:26 +0800

    random

---
+++ unix_c1/Linux_C/rand_demo.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define N 100000
+int a[N];
+
+void gen_random(int upper_bound)
+{
+    int i;
+    for (i = 0; i < N; i++)
+    {
+        a[i] = rand()%upper_bound;
+    }
+}
+
+int howmany(int value)
+{
+    int count = 0, i;
+    for (i = 0; i < N; i++)
+    {
+        if (a[i] == value)
+            ++count;
+    }
+
+    return count;
+}
+
+void print_random()
+{
+    int i;
+    for (i = 0; i < N; i++)
+    {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+}
+
+int main(void)
+{
+    int i;
+    srand(time(NULL));
+    gen_random(10);
+    
+    //print_random();
+    for (i = 0; i < 10; i++)
+        printf("%d\t%d\n", i, howmany(i));
+
+    return 0;
+}
